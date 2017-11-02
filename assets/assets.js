3bd5da5481bff7b98307a6a15946688ed0d8647f
Javi Aguilar <git@itsjavi.com>
2017-11-02 10:19:10 +0100

    Update build with latest master changes

---
+++ assets/assets.js
@@ -0,0 +1,116 @@
+/* This is an auto-generated file. Please use gulp to update it. */
+'use strict';
+window.me.game.assets = {
+  "_files": [
+    {
+      "name": "S01_NewBarkTown",
+      "type": "audio",
+      "src": "assets/bgm/"
+    },
+    {
+      "name": "S01_NewBarkTown",
+      "type": "tmx",
+      "src": "assets/map/S01_NewBarkTown.tmx"
+    },
+    {
+      "name": "overworld",
+      "type": "tsx",
+      "src": "assets/map/overworld.tsx"
+    },
+    {
+      "name": "collide",
+      "type": "audio",
+      "src": "assets/sfx/"
+    },
+    {
+      "name": "door_enter",
+      "type": "audio",
+      "src": "assets/sfx/"
+    },
+    {
+      "name": "door_exit",
+      "type": "audio",
+      "src": "assets/sfx/"
+    },
+    {
+      "name": "escape",
+      "type": "audio",
+      "src": "assets/sfx/"
+    },
+    {
+      "name": "heal",
+      "type": "audio",
+      "src": "assets/sfx/"
+    },
+    {
+      "name": "jump",
+      "type": "audio",
+      "src": "assets/sfx/"
+    },
+    {
+      "name": "select",
+      "type": "audio",
+      "src": "assets/sfx/"
+    },
+    {
+      "name": "frame_gold",
+      "type": "image",
+      "src": "assets/img/gui/frame_gold.png"
+    },
+    {
+      "name": "frame_gold_sm",
+      "type": "image",
+      "src": "assets/img/gui/frame_gold_sm.png"
+    },
+    {
+      "name": "frame_silver",
+      "type": "image",
+      "src": "assets/img/gui/frame_silver.png"
+    },
+    {
+      "name": "frame_silver_sm",
+      "type": "image",
+      "src": "assets/img/gui/frame_silver_sm.png"
+    },
+    {
+      "name": "overworld_tiles",
+      "type": "image",
+      "src": "assets/img/map/overworld_tiles.png"
+    },
+    {
+      "name": "modern_trainer_sprite",
+      "type": "image",
+      "src": "assets/img/sprite/modern_trainer_sprite.png"
+    },
+    {
+      "name": "trainer_sprite",
+      "type": "image",
+      "src": "assets/img/sprite/trainer_sprite.png"
+    }
+  ],
+  "audios": {
+    "S01_NewBarkTown": "S01_NewBarkTown",
+    "collide": "collide",
+    "door_enter": "door_enter",
+    "door_exit": "door_exit",
+    "escape": "escape",
+    "heal": "heal",
+    "jump": "jump",
+    "select": "select"
+  },
+  "maps": {
+    "S01_NewBarkTown": "S01_NewBarkTown"
+  },
+  "tilesets": {
+    "overworld": "overworld"
+  },
+  "images": {
+    "frame_gold": "frame_gold",
+    "frame_gold_sm": "frame_gold_sm",
+    "frame_silver": "frame_silver",
+    "frame_silver_sm": "frame_silver_sm",
+    "overworld_tiles": "overworld_tiles",
+    "modern_trainer_sprite": "modern_trainer_sprite",
+    "trainer_sprite": "trainer_sprite"
+  }
+};

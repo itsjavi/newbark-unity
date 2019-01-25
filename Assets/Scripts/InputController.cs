c453c9477c89ed3f067ca153044c4926ce58eda9
Javi Aguilar <122741+itsjavi@users.noreply.github.com>
2019-01-25 04:42:28 +0100

    NewBark v0.5.0
    - Better original games fidelity for:
      - Movement
      - Collisions
      - Player anchor point
      - Tile colors
      - Map size and tiles
      - Sound effects
    
    - Route 29 map with grass and objects
    - Player's house map
    - Initial door warping engine
    - Use Tilemap 2D Collision component
    - Use Cinemachine camera for confinement and better player following
    - Use PixelPerfect2D camera to preserve aspect-ratio
    - Game screen is 320x288px (2x GBC size)
    - Travis CI binary builds

---
+++ Assets/Scripts/InputController.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+public enum DIRECTION_BUTTON
+{
+    NONE, UP, DOWN, LEFT, RIGHT
+}
+
+public enum ACTION_BUTTON
+{
+    NONE, A, B, START, SELECT
+}
+
+public static class InputController
+{
+    public static ACTION_BUTTON GetPressedActionButton()
+    {
+        if (Input.GetKey(KeyCode.Space) || Input.GetButton("Jump"))
+        {
+            return ACTION_BUTTON.A;
+        }
+        if (Input.GetKey(KeyCode.B) || Input.GetButton("Fire1"))
+        {
+            return ACTION_BUTTON.B;
+        }
+        if (Input.GetKey(KeyCode.S) || Input.GetButton("Fire2"))
+        {
+            return ACTION_BUTTON.START;
+        }
+        if (Input.GetKey(KeyCode.A) || Input.GetButton("Fire3"))
+        {
+            return ACTION_BUTTON.SELECT;
+        }
+        return ACTION_BUTTON.NONE;
+    }
+
+    public static DIRECTION_BUTTON GetPressedDirectionButton()
+    {
+        if (Input.GetKey(KeyCode.UpArrow) || (Input.GetAxis("Vertical") >= 0.5f))
+        {
+            return DIRECTION_BUTTON.UP;
+        }
+        if (Input.GetKey(KeyCode.DownArrow) || (Input.GetAxis("Vertical") <= -0.5f))
+        {
+            return DIRECTION_BUTTON.DOWN;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow) || (Input.GetAxis("Horizontal") <= -0.5f))
+        {
+            return DIRECTION_BUTTON.LEFT;
+        }
+        if (Input.GetKey(KeyCode.RightArrow) || (Input.GetAxis("Horizontal") >= 0.5f))
+        {
+            return DIRECTION_BUTTON.RIGHT;
+        }
+        return DIRECTION_BUTTON.NONE;
+    }
+}

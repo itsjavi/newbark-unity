bb3fd0931262b547eb39d2a4217b110b72a169bd
Route 1 Rodent <18262122+route1rodent@users.noreply.github.com>
2019-10-11 05:51:36 +0200

    [tiles] add initial support for day/night cycle #30

---
+++ Assets/Scripts/NewBark/Tilemap/TileSwap.cs
@@ -0,0 +1,12 @@
+using System;
+using UnityEngine.Tilemaps;
+
+namespace NewBark.Tilemap
+{
+    [Serializable]
+    public class TileSwap
+    {
+        public TileBase changeTile;
+        public TileBase newTile;
+    }
+}
---
+++ Assets/Scripts/NewBark/Tilemap/TileSwapScheduler.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using NewBark.Tilemap;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TileSwapScheduler : MonoBehaviour
+{
+    public string m_Title;
+    public int m_FromHour;
+    public int m_ToHour;
+    public bool m_RestoreOnFinish;
+    public TileSwap[] m_Swaps;
+
+    private bool _swapped = false;
+    private Tilemap[] _tileMaps;
+
+    void Awake()
+    {
+        _tileMaps = GetComponentsInChildren<Tilemap>();
+    }
+
+    void Update()
+    {
+        if (!_swapped && InSchedule())
+        {
+            SwapTiles();
+            Debug.Log("Tiles changed to " + m_Title);
+            return;
+        }
+
+        if (m_RestoreOnFinish && _swapped && !InSchedule())
+        {
+            SwapTilesBack();
+        }
+    }
+
+    bool InSchedule()
+    {
+        return DateTime.Now.Hour >= m_FromHour && DateTime.Now.Hour <= m_ToHour;
+    }
+
+    void SwapTilesBack()
+    {
+        foreach (var tm in _tileMaps)
+        {
+            foreach (var sw in m_Swaps)
+            {
+                tm.SwapTile(sw.newTile, sw.changeTile);
+            }
+        }
+        _swapped = false;
+    }
+
+    void SwapTiles()
+    {
+        foreach (var tm in _tileMaps)
+        {
+            foreach (var sw in m_Swaps)
+            {
+                tm.SwapTile(sw.changeTile, sw.newTile);
+            }
+        }
+        _swapped = true;
+    }
+}

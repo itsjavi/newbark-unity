32b30ce258a80ed6ee357aeae1281aefc99f4a4f
Route 1 Rodent <18262122+route1rodent@users.noreply.github.com>
2019-10-12 08:09:07 +0200

    change: TriggerListener2D collisions now filterable by tags

--- Assets/Scripts/NewBark/Physics/TriggerListener2D.cs
+++ Assets/Scripts/NewBark/Physics/TriggerListener2D.cs
@@ -1,26 +1,44 @@
+using System.Linq;
+using NewBark.Attributes;
 using UnityEngine;
 using UnityEngine.Events;
 
 namespace NewBark.Physics
 {
-    public class TriggerListener2D: MonoBehaviour
+    public class TriggerListener2D : MonoBehaviour
     {
+        [Tag] public string[] tagFilter;
         public UnityEvent onEnter;
         public UnityEvent onStay;
         public UnityEvent onExit;
-        
+
         void OnTriggerEnter2D(Collider2D other)
         {
+            if (tagFilter.Length > 0 && !tagFilter.Any(other.tag.Equals))
+            {
+                return;
+            }
+
             onEnter.Invoke();
         }
 
         void OnTriggerStay2D(Collider2D other)
         {
+            if (tagFilter.Length > 0 && !tagFilter.Any(other.tag.Equals))
+            {
+                return;
+            }
+
             onStay.Invoke();
         }
 
         void OnTriggerExit2D(Collider2D other)
         {
+            if (tagFilter.Length > 0 && !tagFilter.Any(other.tag.Equals))
+            {
+                return;
+            }
+
             onExit.Invoke();
         }
     }

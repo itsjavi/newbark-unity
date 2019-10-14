3098799d9c99e28b7d3919e91737669d3a7ff339
Route 1 Rodent <18262122+route1rodent@users.noreply.github.com>
2019-10-14 05:22:57 +0200

    initial MovementInfo implementation

---
+++ Assets/Scripts/NewBark/Movement/MovementDirection.cs
@@ -0,0 +1,11 @@
+namespace NewBark.Movement
+{
+    public enum MovementDirection
+    {
+        None,
+        Up,
+        Right,
+        Down,
+        Left
+    }
+}
---
+++ Assets/Scripts/NewBark/Movement/MovementInfo.cs
@@ -0,0 +1,63 @@
+using NewBark.Input;
+using UnityEngine;
+
+namespace NewBark.Movement
+{
+    public class MovementInfo
+    {
+        public Vector2 origin;
+        public MovementDirection direction = MovementDirection.None;
+        public int steps;
+        public float speed;
+        public int delay;
+
+        public Vector2 CalculateDestination()
+        {
+            return origin + DirectionToVector(direction) * steps;
+        }
+
+        public static Vector2 DirectionToVector(MovementDirection direction)
+        {
+            switch (direction)
+            {
+                case MovementDirection.Up:
+                    return Vector2.up;
+                case MovementDirection.Right:
+                    return Vector2.right;
+                case MovementDirection.Down:
+                    return Vector2.down;
+                case MovementDirection.Left:
+                    return Vector2.left;
+                default:
+                    return Vector2.zero;
+            }
+        }
+
+        public static MovementDirection VectorToDirection(Vector2 vector)
+        {
+            if (vector == Vector2.up) return MovementDirection.Up;
+            if (vector == Vector2.right) return MovementDirection.Right;
+            if (vector == Vector2.down) return MovementDirection.Down;
+            if (vector == Vector2.left) return MovementDirection.Left;
+
+            return MovementDirection.None;
+        }
+
+        public static MovementDirection ButtonToDirection(InputButton button)
+        {
+            switch (button)
+            {
+                case InputButton.Up:
+                    return MovementDirection.Up;
+                case InputButton.Right:
+                    return MovementDirection.Right;
+                case InputButton.Down:
+                    return MovementDirection.Down;
+                case InputButton.Left:
+                    return MovementDirection.Left;
+                default:
+                    return MovementDirection.None;
+            }
+        }
+    }
+}
